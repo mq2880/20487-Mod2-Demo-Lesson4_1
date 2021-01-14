d80ade04d4d3505667810e685638f015eb99c5f0
Javier Moreno <mq2880@gmail.com>
2021-01-14 10:40:51 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Module 2: Querying and Manipulating Data Using Entity Framework
+
+# Lesson 4: Manipulating Data
+
+### Demonstration: CRUD Operations in Entity Framework
+
+- Abrimos el proyecto de consola CRUD en Visual Studio Code
+- Añadimos el codigo para insertar dos nuevos estudiantes en el main
+- Añadimos el codigo para  incrementar el salario de los profesor del curso de ASP.NET Core
+- Seleccionamos un estudiante llamado Studen_1
+- Guardamos 
+- Ejecutar
+
+
+
+![](./img/Captura1.jpg)
+
---
+++ Starter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using CRUD.Database;
+using CRUD.Models;
+
+namespace CRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+                // Initializing the database and populating seed data
+                DbInitializer.Initialize(context);
+
+                Course ASPCourse = (from course in context.Courses
+                                    where course.Name == "ASP.NET Core"
+                                    select course).Single();
+                Student firstStudent = new Student() { Name = "Thomas Anderson" };
+                Student secondStudent = new Student() { Name = "Terry Adams" };
+                ASPCourse.Students.Add(firstStudent);
+                ASPCourse.Students.Add(secondStudent);
+                ASPCourse.CourseTeacher.Salary += 1000;
+                Student studentToRemove = ASPCourse.Students.FirstOrDefault((student) => student.Name == "Student_1");
+                ASPCourse.Students.Remove(studentToRemove);
+                context.SaveChanges();
+                Console.WriteLine(ASPCourse);
+                Console.ReadLine();
+            }
+        }
+    }
+}
